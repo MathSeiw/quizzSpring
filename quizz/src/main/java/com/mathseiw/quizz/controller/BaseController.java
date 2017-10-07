8f59ae8a9c2f767a1fdecfa388efe9b4e6142d5f
Mathieu Seiwert <seiwert.mathieu@gmail.com>
2017-10-07 20:03:00 +0200

    Ca a marché pour la session

--- quizz/src/main/java/com/mathseiw/quizz/controller/BaseController.java
+++ quizz/src/main/java/com/mathseiw/quizz/controller/BaseController.java
@@ -2,19 +2,15 @@ package com.mathseiw.quizz.controller;
 
 import javax.validation.Valid;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
-
-import org.apache.log4j.Logger;
 
 import com.mathseiw.quizz.model.User;
 
@@ -22,9 +18,12 @@ import com.mathseiw.quizz.model.User;
 @SessionAttributes("user")
 public class BaseController {
 
+    @Autowired
+    private User user;
+    
     @RequestMapping(value = "/start", method = RequestMethod.GET)
     public ModelAndView showForm() {
-        return new ModelAndView("index", "user", new User());
+        return new ModelAndView("index", "user", user);
     }
     @RequestMapping(value = "/start",method = RequestMethod.POST)
     public String requestDone(@Valid @ModelAttribute("user")User user,BindingResult bindingResult,final ModelMap pModel) {
